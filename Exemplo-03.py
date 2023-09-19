6969ab653e38e41903eef3834f9eac108841714a
DANIEL ABRANTES OLINTO <a95054742@rn.senac.local>
2023-09-19 17:06:19 -0300

    corrigido

--- Exemplo-03.py
+++ Exemplo-03.py
@@ -3,7 +3,7 @@ b = float(8)
 c = float(3)
 
 delta = b*b - 4 * a *c
-x1 = (-b + delta**0.5)/2*a
-x2 = (-b - delta**0.5)/2*a
+x1 = (-b + delta**0.5)/(2*a)
+x2 = (-b - delta**0.5)/(2*a)
 
 print(x1, x2)
