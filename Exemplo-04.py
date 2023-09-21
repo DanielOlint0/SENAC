c3fa6b0b2fab8dd6ec0accb06f2116cd6a389fda
DANIEL ABRANTES OLINTO <a95054742@rn.senac.local>
2023-09-21 16:02:36 -0300

    Bhaskara com if corrigido

--- Exemplo-04.py
+++ Exemplo-04.py
@@ -8,11 +8,13 @@ del(c)
 if(delta == 0):
     x = -b/(2*a)
     print(x)
+    del(x)
 elif(delta > 0):
     x1 = (-b + delta**0.5)/(2*a)
     x2 = (-b - delta**0.5)/(2*a)
     print(x1, x2)
+    del(x1,x2)
 else:
     print("x1 e x2 não possuem raizes reais")
-
-del(a,b,x1,x2,x,delta)
+    
+del(a,b,delta)
